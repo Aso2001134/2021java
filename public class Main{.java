3cc9f1d90826addb90ad75baeb70a393ead35b74
Aso2001134 <83264678+Aso2001134@users.noreply.github.com>
2021-07-30 22:45:12 +0900

    Add files via upload

---
+++ public class Main{.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("戦艦ゲーム");
+        System.out.println("入力してください");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+    }
+}
